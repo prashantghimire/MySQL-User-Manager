18fc9e72425de745e39f7ae48ee2ff9b18404a59
Prashant Ghimire <prashant.ghimire@selu.edu>
2015-10-19 17:12:55 -0500

    Converted the current project into CodeIgniter project. :)

---
+++ application/views/dashboard/add.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <h1>Add!</h1>
+
+    <form action="core/add-users.php" method="post">
+        <textarea name="users" id="users" cols="50" rows="30"></textarea>
+        <input class="block btn btn-go" type="submit" value="Add Users" />
+    </form>
+</div>
